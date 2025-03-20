4bf49932fa2cd375327224a89ba924a931b80107
Tushar Chindhe <tusharchindhe2002@gmail.com>
2025-03-20 20:28:27 +0530

    Load tasks from localStorage if any

--- js/todo.js
+++ js/todo.js
@@ -1,3 +1,5 @@
 const todoInput = document.getElementById('todo-input');
 const addBtn = document.getElementById('add-btn');
 const todoList = document.getElementById('todo-list');
+
+document.addEventListener('DOMContentLoaded', loadTasks);
